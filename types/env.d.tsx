85a25b6d8ae823d345d848a7039ba0d6941d7668
Derek Penaloza <82470734+derekdip@users.noreply.github.com>
2023-12-13 00:50:01 -0800

    Update env.d.tsx

--- types/env.d.tsx
+++ types/env.d.tsx
@@ -1,5 +1,4 @@
-declare module '@env' {
-    export const EXPO_PUBLIC_MAZEURL: string;
+declare module '@env' 
     export const PHOTON_APP_ID: string;
     export const PHOTON_APP_VERSION: string;
 }
